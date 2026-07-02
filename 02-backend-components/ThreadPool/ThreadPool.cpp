ceaa5ace24f4639a0e26a110f189b8d7ab15f1cc
zymJDFH <2994059821@qq.com>
2026-07-02 17:44:07 +0800

    docs(threadpool)

---
+++ 02-backend-components/ThreadPool/ThreadPool.cpp
@@ -0,0 +1,59 @@
+#include "ThreadPool.h"
+ThreadPool::ThreadPool(size_t thread_count)
+{
+    if (thread_count == 0) {
+        throw std::invalid_argument("thread_count must be greater than 0");
+    }
+
+    for(size_t i=0;i<thread_count;i++){
+        workers_.emplace_back(&ThreadPool::workerLoop, this);
+    }
+}
+
+ThreadPool::~ThreadPool()
+{
+    {
+        std::lock_guard<std::mutex>lock(mutex_);
+        stopping_=true;
+    }
+
+    cv_.notify_all();
+
+    for(auto&worker:workers_){
+        if(worker.joinable()){
+            worker.join();
+        }
+    }
+
+}
+
+void ThreadPool::enqueue(std::function<void()>task){
+    {
+        std::lock_guard<std::mutex>lock(mutex_);
+        if (stopping_) {
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+        }
+        tasks_.push(std::move(task));
+    }
+    cv_.notify_one();
+}
+
+void ThreadPool::workerLoop(){
+    while(true){
+        std::function<void()>task;
+        {
+            std::unique_lock<std::mutex>lock(mutex_);
+            
+            cv_.wait(lock,[this]{
+                return stopping_||!tasks_.empty();
+            });
+
+            if(stopping_&&tasks_.empty()){
+                return ;
+            }
+            task=std::move(tasks_.front());
+            tasks_.pop();
+        }
+        task();
+    }
+}
---
+++ 02-backend-components/ThreadPool/ThreadPool.h
@@ -0,0 +1,35 @@
+#pragma once
+
+
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <functional>
+#include <thread>
+#include <condition_variable>
+#include <stdexcept>
+class ThreadPool
+{
+private:
+    std::vector<std::thread>workers_;
+    std::queue<std::function<void()>>tasks_;
+    std::mutex mutex_;
+    std::condition_variable cv_;
+    bool stopping_=false;
+    
+public:
+    explicit ThreadPool(size_t thread_count);
+    ~ThreadPool();
+
+    ThreadPool(const ThreadPool&)=delete;
+    ThreadPool& operator=(const ThreadPool&)=delete;
+
+    void enqueue(std::function<void()>task);
+
+private:
+    void workerLoop();
+
+};
+
+
+
